9b1cb433abdb8a3c42cd459a25db12f65cb7a944
Jetmir007 <jeku07001@utb.vaxjo.se>
2025-01-24 09:06:59 +0100

    Sort

--- Program.cs
+++ Program.cs
@@ -32,62 +32,63 @@ void Insertion(ref int[] arr){
     }
 }
 
-static void Merge(int[] arr, int l, int m, int r){
-    int sub1 = m-1+1;
-    int sub2 = r-m;
-
-    int[] Left = new int[sub1];
-    int[] Right = new int[sub2];
-    int i, j;
-
-    for (i = 0; i < sub1; i++)
+static void Merge(int[] arr, int l, int m, int r)
     {
-        Left[i] = arr[l+1];
-    }
-    for (j = 0; j < sub2; j++)
-    {
-        Right[j] = arr[m+1+j];
-    }
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-    j = 0;
-    i = 0;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j;
 
-    int k = 1;
+        for (i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
-while(i<sub1 && j<sub2){
-    if (Left[i] <= Right[j]) {
-            arr[k] = Left[i];
+        i = 0;
+        j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
             i++;
+            k++;
         }
-        else {
-            arr[k] = Right[j];
+
+
+        while (j < n2) {
+            arr[k] = R[j];
             j++;
+            k++;
         }
-    k++;
-}
-
-    while(i<sub1){
-        arr[k] = Left[i];
-        i++;
-        k++;
     }
 
-    while(j<sub2){
-        arr[k] = Right[j];
-        j++;
-        k++;
-    }
-}
+    static void MergeSort(int[] arr, int l, int r)
+    {
+        if (l < r) {
 
-static void Mergesort(int[] arr, int l, int r){
-    if(l<r){
-        int m = l+(r-l) /2;
-        Mergesort(arr, l, m);
-        Mergesort(arr, m+1, r);
+            int m = l + (r - l) / 2;
 
-        Merge(arr, l, m, r);
+            MergeSort(arr, l, m);
+            MergeSort(arr, m + 1, r);
+
+            Merge(arr, l, m, r);
+        }
     }
-}
+
 
 static int Partition(int[] arr, int l, int h){
     int p = arr[h];
@@ -124,14 +125,14 @@ static void Quick(int[] arr, int l, int h){
 void Main()
 {
     Stopwatch watch = new Stopwatch();
-    int[] tal = new int[100000];
+    int[] tal = new int[10];
     for (int i = 0; i <= tal.Length-1; i++)
     {
         Random rng = new Random();
         tal[i] = rng.Next(0, 1000000);
     }
     watch.Start();
-    Quick(tal, 0, tal.Length-1);
+    MergeSort(tal, 0, tal.Length-1);
     watch.Stop();
     foreach(int n in tal){
         Console.Write(n + " ");
